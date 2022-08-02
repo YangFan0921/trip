f1eeda5048a1fb56b942fda4b3079fa00af1a60c
aFan <2388408281@qq.com>
2022-08-02 23:23:39 +0800

    删除com.sun.deploy.net包

--- src/main/java/com/journey/trip/controller/UserController.java
+++ src/main/java/com/journey/trip/controller/UserController.java
@@ -2,7 +2,6 @@ package com.journey.trip.controller;
 
 import com.journey.trip.entity.User;
 import com.journey.trip.mapper.UserMapper;
-import com.sun.deploy.net.HttpResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
